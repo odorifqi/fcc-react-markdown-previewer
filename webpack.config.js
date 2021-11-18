10ca43ef060b9b49b6d68060f26e2b2904721804
odorifqi <09021281621065@student.unnsri.ac.id>
2021-11-18 21:21:29 +0700

    init

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: path.resolve(__dirname, "./src/index.js"),
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.css$/i,
+        exclude: /node_modules/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"],
+  },
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "bundle.js",
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+  devServer: {
+    static: path.resolve(__dirname, "./dist"),
+    hot: true,
+  },
+};
